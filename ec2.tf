2a29084b44539be88151f3bcc49711de83756492
DileepBandi <140000109+DileepBandi@users.noreply.github.com>
2024-04-19 21:04:59 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-region = "eu-west-2"
+  region = "eu-west-2"  # Specify your desired AWS region
 }
 
-resource "aws_instance" "one" {
-  ami             = "ami-09cce85cf54d36b29"
-  instance_type   = "t2.micro"
-  key_name        = "project_keypair"
+resource "aws_instance" "example" {
+  ami           = "ami-09cce85cf54d36b29"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"                # Specify your desired instance type
 }
