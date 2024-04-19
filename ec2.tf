f61a8a2346c1f89511bb895e1cac0c53406c33e4
DileepBandi <140000109+DileepBandi@users.noreply.github.com>
2024-04-19 20:37:55 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,5 +6,4 @@ resource "aws_instance" "one" {
   ami             = "ami-09cce85cf54d36b29"
   instance_type   = "t2.micro"
   key_name        = "project_keypair"
-  vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-2"
