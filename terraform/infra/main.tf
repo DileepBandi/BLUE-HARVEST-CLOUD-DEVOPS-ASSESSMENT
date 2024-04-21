cf44af3579a55602469a55d3d05298c806ffb13f
DileepBandi <140000109+DileepBandi@users.noreply.github.com>
2024-04-21 22:19:29 +0100

    Update main.tf

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -1,1 +1,163 @@
+# main.tf
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
 
+
+resource "aws_iam_role" "ecsTaskExecutionRole1" {
+  name               = "ecsTaskExecutionRole1"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole1.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name                 = "app-repo-terraform"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "app-cluster-terraform" # Name your cluster here
+}
+
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "app-task-teraform-pratice" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole1.arn}"
+}
+
+
+
+# Provide a reference to your default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Provide references to your default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  # Use your own region here but reference to subnet 1a
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  # Use your own region here but reference to subnet 1b
+  availability_zone = "us-east-1b"
+}
+
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev" #load balancer name
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  # security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+
+
+# Create a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" #  load balancer
+  port              = "8080"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # target group
+  }
+}
+
+
+
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#Log the load balancer app URL
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
+output "ecr_url" {
+  value = aws_ecr_repository.app_ecr_repo.repository_url
+}
