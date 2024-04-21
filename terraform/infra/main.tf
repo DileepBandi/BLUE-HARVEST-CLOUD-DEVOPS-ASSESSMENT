4f87232e3c1836ffcbb71aa86ed9eb61ab6507af
DileepBandi <140000109+DileepBandi@users.noreply.github.com>
2024-04-21 22:41:04 +0100

    changing region back to us-east-1

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -1,6 +1,6 @@
 # main.tf
 provider "aws" {
-  region  = "eu-west-2"
+  region  = "us-east-1"
   #profile = "default"
 }
 
@@ -75,12 +75,12 @@ resource "aws_default_vpc" "default_vpc" {
 # Provide references to your default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   # Use your own region here but reference to subnet 1a
-  availability_zone = "eu-west-2a"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
   # Use your own region here but reference to subnet 1b
-  availability_zone = "eu-west-2b"
+  availability_zone = "us-east-1b"
 }
 
 
