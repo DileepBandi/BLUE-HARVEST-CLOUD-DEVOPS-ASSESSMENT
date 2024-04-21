3761f19cc0eb2b4507d7742365415f4372dbeb5f
DileepBandi <140000109+DileepBandi@users.noreply.github.com>
2024-04-21 22:20:51 +0100

    Create main.tf

---
+++ terraform/ecs-service/main.tf
@@ -0,0 +1,19 @@
+resource "aws_ecs_service" "app_service" {
+  name            = "app-service-terraform"     # Name the service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"   # Reference the created Cluster
+  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Set up the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
+    container_name   = "app-first-task"
+    container_port   = 8080 # Specify the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true     # Provide the containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
